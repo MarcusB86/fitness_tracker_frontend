1d93740404436daa33ae8fe607923cf7855d35ef
MarcusB86 <marcusb08@gmail.com>
2024-07-02 12:21:46 -0400

    setup Components Pages Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import { Router,Route, Routes } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
+  useEffect(() => {
+    fetch(`${API}/workouts`)
+    .then((res) => {
+      return res.json()
+    })
+    .then(resJSON => console.log(resJSON))
+  }, [])
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Router>
+        <NavBar/>
+        <main>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/workouts' element={<Index/>} />
+            <Route path='/workouts/:id' element={<Show/>} />
+            <Route path='/workouts/:id/edit' element={<Edit/>} />
+            <Route path='workouts/new' element={<New/>} />
+            <Route path='*' element={<FourOFour/>} />
+          </Routes>
+        </main>
+      </Router>
+    </div>
+  
+
+
   )
 }
 
